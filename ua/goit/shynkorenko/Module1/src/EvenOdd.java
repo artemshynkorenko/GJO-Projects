c9446b6a592bdc7f23a5defae703e20bb2efe334
Artem Shynkorenko <artem.shynkorenko@gmail.com>
2016-03-04 21:19:44 +0100

    Module2

---
+++ ua/goit/shynkorenko/Module1/src/EvenOdd.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Artem on 01.03.2016.
+ */
+package ua.goit.shynkorenko.module1;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args){
+        evod();
+    }
+    public static void evod(){
+            Scanner console = new Scanner(System.in);
+            System.out.printf("Введите любое число : ");
+            int number = console.nextInt();
+            if ((number % 2) == 0) {
+                System.out.printf("Число %d четное число! \n", number);
+            } else {
+                System.out.println("Это число нечетное!");
+            }
+        }
+    }
---
+++ ua/goit/shynkorenko/Module1/src/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+package ua.goit.shynkorenko.module1;
+
+public class FirstJavaProgram {
+	public static void main(String [] args) {
+		System.out.println ("Hi, I'm your first Java program! Stay tuned!");
+	}
+}
+//залито на github с помощью git bash
